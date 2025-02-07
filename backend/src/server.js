b8780de7410505b9d8a0c6f9a8a7f6f955e95ea5
Abhishek Shaw <145495694+Abhishek26102002@users.noreply.github.com>
2025-02-07 15:33:39 +0530

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -20,7 +20,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(cookieParser());
 
-app.use(cors({ origin: "https://mern-04-echoes.vercel.app/", credentials: true }));
+app.use(cors({ origin: "https://mern-04-echoes.vercel.app", credentials: true }));
 
 
 app.get("/", (req, res) => {
