deff5f460ac46e1467ede4c2e596042a89f1078f
Abhishek Shaw <145495694+Abhishek26102002@users.noreply.github.com>
2025-02-07 16:30:22 +0530

    Update socket.js

--- backend/src/config/socket.js
+++ backend/src/config/socket.js
@@ -5,11 +5,10 @@ import express from "express";
 const app = express();
 const server = http.createServer(app);
 
-const allowedOrigins = ["https://mern-04-echoes.vercel.app"];
 
 const io = new Server(server, {
  cors: {
-    origin: allowedOrigins,
+    origin: "https://mern-04-echoes.vercel.app",
   },
 });
 
