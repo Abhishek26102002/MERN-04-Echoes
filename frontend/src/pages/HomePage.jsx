b390d24d345494b3287b0893fa61d099ff6ee5b0
Abhishek Shaw <145495694+Abhishek26102002@users.noreply.github.com>
2025-03-02 23:44:04 +0530

    Update HomePage.jsx
    fix white border issue

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -9,7 +9,7 @@ const HomePage = () => {
   const isLargeScreen = useMediaQuery({ minWidth: 1024 });
   const { selectedUser } = useChatStore();
   return (
-    <div className="h-screen bg-base-200 border">
+    <div className="h-screen">
       <div className="flex items-center justify-center pt-16 px-2 sm:pt-20 sm:px-4">
         <div className="bg-base-100 rounded-lg shadow-cl w-full h-[calc(100vh-6rem)]">
           <div className="flex h-full rounded-lg overflow-hidden">
